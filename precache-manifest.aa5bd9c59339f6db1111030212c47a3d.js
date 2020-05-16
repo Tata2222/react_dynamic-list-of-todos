0e9caa798110a1d2d8b7fb4011a86561c2f1367d
Tata2222 <tataolcha@gmail.com>
2020-05-16 19:11:00 +0300

    Updates

---
+++ precache-manifest.aa5bd9c59339f6db1111030212c47a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d3c52fd128f9437cae7db3e2f229b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d1120aaa8877ceaf6259",
+    "url": "./static/css/main.73f98911.chunk.css"
+  },
+  {
+    "revision": "2a1d051883d44bfb1343",
+    "url": "./static/js/2.5e39cbed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5e39cbed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1120aaa8877ceaf6259",
+    "url": "./static/js/main.1f654e99.chunk.js"
+  },
+  {
+    "revision": "319a3c468d8ecd2a5698",
+    "url": "./static/js/runtime-main.e5e6300a.js"
+  }
+]);
